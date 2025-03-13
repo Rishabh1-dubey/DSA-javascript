1909b0cbe4f22f811c06176e082f5a7b8387ea96
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-03-13 14:01:28 +0530

    day -2 done

---
+++ day-2 condtional/swtich-statement/index.js
@@ -0,0 +1,11 @@
+let num = Math.round((0.1 + 0.2) *10)/10;
+switch(num){
+    case 0.3 :console.log(" first case is runnging ")
+    break
+
+    case 0.5 : console.log("second case is running")
+    break
+    
+
+    default : console.log("namaste")
+}
