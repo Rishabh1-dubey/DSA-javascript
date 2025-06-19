69aba38e34b3a4f1b20eca037986bf2337b5db86
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-06-19 07:47:02 +0530

    pratised on bestitme to buy an stock

---
+++ array-day4/buy_sell_stock.js
@@ -0,0 +1,11 @@
+
+let prices =[4,5,7,3,8,5]
+let Maxprice =0;
+let min = prices[0]
+
+for(let  i=0; i<prices.length; i++){
+    if(prices[i]<min) min = prices[i]
+    let profit = prices[i]-min;
+    Maxprice = Math.max(Maxprice,profit)
+}
+console.log("The Best time Buy and Stock is ",Maxprice)
