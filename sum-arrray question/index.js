626fd42c8b19ad3f2f6c7d9c0e9bc380fb44f9b4
Rishabh1-dubey <rishabhdubey006@gmail.com>
2026-01-16 15:13:11 +0530

    done with preactise array

--- sum-arrray question/index.js
+++ sum-arrray question/index.js
@@ -12,8 +12,9 @@
 //   return sum;
 // }
 
-// console.log(sumEvenArray(arr));
+// console.log(sumEvenArray(arr));-
 
+//----------------------------------------------------------------------------------------------------
 // Count Frequency
 // const str = ["a", "b", "a", "c", "b", "a"];
 
@@ -28,20 +29,47 @@
 //   return obj;
 // }
 // console.log(countFrequeny(str));
-
+// -------------------------------------------------------------------------------------------
 //remove duplicate from array
-const arr = [1, 2, 3, 2, 4, 1, 5];
+// const arr = [1, 3, 4, 1, 2, 3, 2, 4, 1, 5];
 
-function removeDuplicate(arr) {
-  let newArr = new Set();
-  let result = [];
+// function removeDuplicate(arr) {
+//   let newArr = new Set();
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!newArr.has(arr[i])) {
+//       newArr.add(arr[i]);
+//       result.push(arr[i]);
+//     }
+//   }
+//   return result;
+// }
+// console.log(removeDuplicate(arr));
+
+// -----------------------------------------------------------------------------------------------
+//remove duplicated from sorter array
+// const arr = [1, 2, 3, 3, 4, 5, 6, 6, 7, 8];
+
+// function removSortedArray(arr) {
+//   let x = 0;
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] !== arr[x]) {
+//       x++;
+//       arr[x] = arr[i];
+//     }
+//   }
+//   return arr.slice(0, x + 1);
+// }
+// console.log(removSortedArray(arr));
+
+//find missing number using sorted array
+
+const arr = [1, 2, 3, 5, 6, 7];
+//o/p" 4
+
+function missingNumber(arr) {
   for (let i = 0; i < arr.length; i++) {
-    if (!newArr.has(arr[i])) {
-      newArr.add(arr[i]);
-      result.push(arr[i]);
-    }
+    if (arr[i + 1] !== arr[i] + 1) return arr[i] + 1;
   }
-  return result;
 }
-
-console.log(removeDuplicate(arr));
+console.log(missingNumber(arr));
